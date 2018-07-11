7e45e4423a5ab18a632fd08c6f5522789c31896d
miguelmontiel30 <31743982+miguelmontiel30@users.noreply.github.com>
2018-07-11 11:52:58 -0500

    Primer Commit del sistema
    Carga de librerias BootStrap 3

---
+++ js/plugins.js
@@ -0,0 +1,18 @@
+/* Plugin para Aside POSTS*/
+<div id="fb-root"></div>
+window.fbAsyncInit = function() {
+  FB.init({
+    appId            : '626680674358402',
+    autoLogAppEvents : true,
+    xfbml            : true,
+    version          : 'v3.0'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "https://connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
